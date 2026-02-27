92abb8a827c561c896682be76e080f879616d04a
Huseyin Yatar <huseyin.yatar@ogr.sakarya.edu.tr>
2026-02-27 16:43:37 +0300

    mock movie list and selected sidebar UI

--- movie-app/starter/movie-app/src/App.jsx
+++ movie-app/starter/movie-app/src/App.jsx
@@ -1,41 +1,84 @@
-const movie_list = [
+import { useState } from "react";
+import { movie_list } from "./data.js";
+
+const selected_list = [
   {
     Id: "769",
     Title: "GoodFellas",
     Year: "1990",
     Poster:
       "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-  },
-  {
-    Id: "120",
-    Title: "The Lord of the Rings",
-    Year: "2001",
-    Poster:
-      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-  },
-  {
-    Id: "27205",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://image.tmdb.org/t/p/original/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg",
-  },
-  ,
-  {
-    Id: "105",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
+    rating: 8.4,
   },
 ];
 
 function App() {
+  const [movies, setMovies] = useState(movie_list);
+  const [selectedMovies, setselectedMovies] = useState(selected_list);
+
   return (
     <>
-      <h1>Movie App</h1>
+      <nav className="bg-primary text-white p-2">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-4">Movie App</div>
+            <div className="col-4">
+              <input type="text" className="form-control" />
+            </div>
+            <div className="col-4 text-end">
+              <strong>5</strong> eleman se�ildi
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <main className="container ">
+        <div className="row ">
+          <div className="col-md-9  ">
+            <div className="movie-list">
+              <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
+                {movie_list.map((movie) => (
+                  <div className="card" key={movie.Id}>
+                    <img src={movie.Poster} alt={movie.Title} />
+                    <div className="card-body">
+                      <h6>{movie.Title}</h6>
+                      <div className="d-flex align-items-center">
+                        <i className="bi bi-calendar2-date"></i>
+                        <span className="ms-2">{movie.Year}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3  ">
+            <div className="selected-move-list">
+              {selectedMovies.map((selected_movie, index) => (
+                <div className="card mb-2" key={index}>
+                  <div className="row">
+                    <div className="col-4">
+                      <img
+                        src={selected_movie.Poster}
+                        className="img-fluid rounded-start"
+                      />
+                    </div>
+                    <div className="col-8">
+                      <div className="card-body">
+                        <h6 className="card-title">{selected_movie.Title}</h6>
+                        <i className="bi bi-star-fill text-warning"></i>
+                        <span className="ms-2">{selected_movie.rating}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
